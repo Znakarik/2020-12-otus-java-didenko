a8add9757a08ecae57678178cc2c1b47f084b51a
znakarik <znakarik@icloud.com>
2021-01-25 08:31:28 +0300

    HW-02-collections - DONE

---
+++ hw02-collections/src/main/java/ru/znakarik/otus/core/CustomerReverseOrder.java
@@ -0,0 +1,18 @@
+package ru.znakarik.otus.core;
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class CustomerReverseOrder {
+
+    private final Deque<Customer> customers = new ArrayDeque<>();
+
+    public void add(Customer customer) {
+        customers.push(customer);
+    }
+
+    public Customer take() {
+        return customers.pop();
+    }
+}
